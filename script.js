aebed3ca63574621d09d083ecb70001c09477230
Yash Hulle <150221594+INFINITYASH3699@users.noreply.github.com>
2024-07-24 19:32:19 +0530

    Display housecards via js for ease and visit function enabled for more details

--- script.js
+++ script.js
@@ -22,3 +22,57 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleSwitch.addEventListener("click", toggleDarkMode);
   applyDarkModePreference();
 });
+
+function displayHouses() {
+  const houseList = document.getElementById("houseList");
+  houseList.innerHTML = "";
+  houses.forEach((house, index) => {
+    const houseCard = `
+    <div class="col-3 card" onclick="houseInfo(${index});">
+            <img
+              src="${house.img}"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <div class="card-content">
+                <h5>${house.name}</h5>
+                <p>
+                  ${house.desp}
+                </p>
+              </div>
+              <div class="card-btn text-end">
+                <button class="btn btn-outline-dark" onclick="houseInfo(${index});">Visit</button>
+              </div>
+            </div>
+          </div>
+    `;
+    houseList.innerHTML += houseCard;
+  });
+}
+
+function houseInfo(index) {
+  window.location.href = `houseDetails.html?houseIndex=${index}`;
+}
+
+function getQueryParams() {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    houseIndex: params.get("houseIndex")
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const { houseIndex } = getQueryParams();
+  const house = houses[houseIndex];
+  if (house) {
+    document.getElementById("house-name").textContent = house.name;
+    document.getElementById("house-img").src = house.img;
+    document.getElementById("house-img").alt = house.name;
+    document.getElementById("house-desp").textContent = house.desp;
+  } else {
+    document.getElementById("house-name").textContent = "House not found";
+  }
+});
+
+displayHouses();
