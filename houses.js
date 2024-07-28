77d17fbe7c993ac0876c62f5cf68d8cf270dcc1a
Sangram Chougule <chougulesangram3@gamil.com>
2024-07-28 23:37:07 +0530

    About update

---
+++ houses.js
@@ -0,0 +1,94 @@
+const houses = [
+  {
+    id: 1,
+    img: "https://assets-news.housing.com/news/wp-content/uploads/2021/12/21163320/Types-of-houses-in-India-Studio-house-shutterstock_1086948224.jpg",
+    name: "RK or studio room",
+    desp: "RK is the short form for room-kitchen, and is also known as a studio apartment. These houses, mostly seen in metro cities, do not have a separate bedroom or a living room.",
+    price: "Price 10 lkh to 70 lkh",
+    space: "1 RK flat is basically a 1 room kitchen set. This room is used as a living room cum dining room with a kitchen and a toilet. 1 RK is ideal for single people or two people. The area of RK flat is about 300 to 400 sq ft.",
+    matr: "Materials : Fiberglass, Mineral, woolFoam, High-density, fiberglassFoam, Wood, Plastic, Specialized foam, Mass Loaded Vinyl (MLV), Green Glue Compound, Acoustic Sealant, Soundproof Drwall",
+    con: "Yash Hulle : 99999999999"
+
+  },
+  {
+    id: 2,
+    img: "https://assets-news.housing.com/news/wp-content/uploads/2021/12/21163310/Types-of-houses-in-India-Penthouse-shutterstock_36946348.jpg",
+    name: "Penthouse",
+    desp: "A penthouse is a luxury house on the top floor of a premium building. Penthouses in India are associated with exclusivity and status symbol.",
+    price: "Price $200-$400 s/f to $400-$1,000 s/f",
+    space: "Space : 1,500 to 5,000+ square feet, depending on the luxury and location.",
+    matr: "Materials : Steel, Reinforced, Flooring, Walls, Ceilings, Windows, Doors, Kitchens, Bathrooms, Lighting",
+    con: "Shyam Patil : 999999999"
+  },
+  {
+    id: 3,
+    img: "https://assets-news.housing.com/news/wp-content/uploads/2021/12/21163255/Types-of-houses-in-India-Bungalow-shutterstock_52187887.jpg",
+    name: "Bungalow",
+    desp: "Bungalow type of houses have a large veranda, a low-pitched roof and a single-storey or one-and-a-half-storey design.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 4,
+    img: "https://assets-news.housing.com/news/wp-content/uploads/2021/12/21163327/Types-of-houses-in-India-Villa-shutterstock_782466463.jpg",
+    name: "Villa",
+    desp: "A villa type of house in India is like a luxury home that boasts of all the state-of-the-art amenities.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 5,
+    img: "https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/Types%20Of%20Homes/Stock-Gray-Single-Family-Home-AdobeStock_345676065-copy.jpeg",
+    name: "Tiny Home",
+    desp: "Tiny Home. Small rectangular wooden home. Tiny home popularity has boomed in recent years, spurring what's known as the “tiny home movement.”",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 6,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Anne_Hathaways_Cottage_and_gardens_15g2006.jpg/800px-Anne_Hathaways_Cottage_and_gardens_15g2006.jpg",
+    name: "Cabin",
+    desp: "Cabin. A small, rustic house traditionally made with wood and logs. The cabin is most often located far from surrounding cities, towns, or any populated areas.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 7,
+    img: "https://www.houzeo.com/blog/wp-content/uploads/2023/07/Farmhouse.webp",
+    name: "Farmhouses",
+    desp: "Farmhouses. Located away from the hustle and bustle of cities, towards the outskirts, farmhouses generally include small dwellings that are nestled amidst.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 8,
+    img: "https://www.houzeo.com/blog/wp-content/uploads/2023/07/Cape-Cod.webp",
+    name: "Cape Cod homes",
+    desp: "Cape Cod homes, named after the vacation destination in Massachusetts, reflect a quaint New England aesthetic.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+  {
+    id: 9,
+    img: "https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-19.jpg",
+    name: "Treehouse",
+    desp: "Treehouse. Nestled within the branches of trees, treehouses offer a unique residential experience in forested areas.",
+    price: "",
+    space: "",
+    matr: "",
+    con: ""
+  },
+
+];
---
+++ script.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.querySelector(".toggle-switch");
+
+  function applyDarkModePreference() {
+    if (localStorage.getItem("darkMode") === "enabled") {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }
+
+  function toggleDarkMode() {
+    if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "disabled");
+    } else {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "enabled");
+    }
+  }
+
+  toggleSwitch.addEventListener("click", toggleDarkMode);
+  applyDarkModePreference();
+});
+
+function displayHouses() {
+  const houseList = document.getElementById("houseList");
+  houseList.innerHTML = "";
+  houses.forEach((house, index) => {
+    const houseCard = `
+    <div class="col-3 card" onclick="houseInfo(${index});">
+            <img
+              src="${house.img}"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <div class="card-content">
+                <h5>${house.name}</h5>
+                <p>
+                  ${house.desp}
+                </p>
+              </div>
+              <div class="card-btn text-end">
+                <button class="btn btn-outline-dark" onclick="houseInfo(${index});">Visit</button>
+              </div>
+            </div>
+          </div>
+    `;
+    houseList.innerHTML += houseCard;
+  });
+}
+
+function houseInfo(index) {
+  window.location.href = `houseDetails.html?houseIndex=${index}`;
+}
+
+function getQueryParams() {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    houseIndex: params.get("houseIndex")
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const { houseIndex } = getQueryParams();
+  const house = houses[houseIndex];
+  if (house) {
+    document.getElementById("house-name").textContent = house.name;
+    document.getElementById("house-img").src = house.img;
+    document.getElementById("house-img").alt = house.name;
+    document.getElementById("house-desp").textContent = house.desp;
+    document.getElementById("house-price").textContent = house.price;
+    document.getElementById("house-space").textContent = house.space;
+    document.getElementById("house-matr").textContent = house.matr;
+    document.getElementById("house-con").textContent = house.con;
+
+
+  } else {
+    document.getElementById("house-name").textContent = "House not found";
+  }
+});
+
+displayHouses();
